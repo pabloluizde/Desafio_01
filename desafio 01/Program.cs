6198439e1b6986f14dff9f9aa15659d7c0dc3933
pabloluizde <pablo2006_luiz@hotmail.com>
2021-10-02 13:53:26 -0300

    criei um novo programa devido ao erro que estava causando

--- desafio 01/Program.cs
+++ desafio 01/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Desafio_01
+namespace desafio_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
